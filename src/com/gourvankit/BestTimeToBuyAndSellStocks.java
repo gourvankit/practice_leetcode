a963cf6845822dfaffe9c1bf2ff0460dcc8533f9
gourvankit <gourvankit23@gmail.com>
2023-02-25 15:07:57 +0530

    first

---
+++ src/com/gourvankit/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,27 @@
+package com.gourvankit;
+
+public class BestTimeToBuyAndSellStocks {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int lptr=0;
+        int rptr=1;
+        for(int i=0;i<prices.length-1;i++) {
+            if(prices[rptr]>prices[lptr]){
+                int pf=prices[rptr]-prices[lptr];
+                if(pf>maxProfit){
+                    maxProfit=pf;
+                }
+
+            }else{
+                lptr=rptr;
+            }
+            rptr++;
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/com/gourvankit/SubsetsII.java
@@ -0,0 +1,28 @@
+package com.gourvankit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+    static public void recurr(int[] nums,int index,List<List<Integer>> mylists,List<Integer> mylist){
+        mylists.add(new ArrayList<>(mylist));
+        for(int i=index;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]) continue;
+            mylist.add(nums[i]);
+            recurr(nums,i+1,mylists,mylist);
+            mylist.remove(mylist.size()-1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> mylists=new ArrayList<>();
+        List<Integer> mylist=new ArrayList<>();
+        Arrays.sort(nums);
+        recurr(nums,0,mylists,mylist);
+        return mylists;
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
